f80a27161ed6f1385b91b4226395d9fa1fa99ecc
mille-f <f11235tetra@gmail.com>
2018-01-03 22:16:43 +0900

    画像urlの取得テスト

--- main.rb
+++ main.rb
@@ -1,4 +1,15 @@
 require 'twitter'
 require './client'
 
-$client.update("test")
+# max_id = $client.home_timeline.first.id
+
+tweets = $client.user_timeline("flower_staff", {count: 200})
+img = tweets.flat_map {|s| s.media}.map {|m| m.media_url.to_s}
+
+$client.user_timeline("flower_staff", {count: 200}).each do |timeline|
+  txt = $client.status(timeline.id).text
+  if txt.include?("花騎士紹介")
+    p $client.status(timeline.id).media
+#     puts txt
+  end
+end
