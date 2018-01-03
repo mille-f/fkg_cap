a8ff7aa25f4d137ad769468ceedac22deb53f506
mille-f <f11235tetra@gmail.com>
2018-01-04 05:17:27 +0900

    full_textを取得できるようにした

--- main.rb
+++ main.rb
@@ -3,13 +3,12 @@ require './client'
 
 # max_id = $client.home_timeline.first.id
 
-tweets = $client.user_timeline("flower_staff", {count: 200})
-img = tweets.flat_map {|s| s.media}.map {|m| m.media_url.to_s}
-
 $client.user_timeline("flower_staff", {count: 200}).each do |timeline|
-  txt = $client.status(timeline.id).text
+  txt = timeline.text
+#   timeline.media.each do |m|
+#     puts m.media_url
+#   end
   if txt.include?("花騎士紹介")
-    p $client.status(timeline.id).media
-#     puts txt
+    puts $client.status(timeline.id, tweet_mode: "extended").attrs
   end
 end
